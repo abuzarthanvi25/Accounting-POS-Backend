6d2ac322ede26d34076a7936011caa65090fad6d
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2023-06-27 16:08:39 +0500

    CHANGED: Anchors to Notes

--- routes/index.js
+++ routes/index.js
@@ -17,28 +17,28 @@ router.post("/", (req, res) => {
 
 // < ============================================== MAIN ROUTES ============================================== >
 
-//ANCHOR - CUSTOMER ROUTES ===================== >
+//NOTE - CUSTOMER ROUTES ===================== >
 router.get("/api/customer", CustomerController.GetAllCustomers)
 router.get("/api/find/customer", CustomerController.GetCustomerByName)
 router.post("/api/customer", CustomerController.AddCustomer)
 router.delete("/api/customer", CustomerController.DeleteCustomer)
 
-//ANCHOR - PRODUCT ROUTES ===================== >
+//NOTE - PRODUCT ROUTES ===================== >
 router.get("/api/products", ProductController.GetAllProducts)
 router.post("/api/products", ProductController.AddProduct)
 
-//ANCHOR - SUPPLIER ROUTES ===================== >
+//NOTE - SUPPLIER ROUTES ===================== >
 router.get("/api/suppliers", SupplierController.GetAllSuppliers)
 router.get("/api/find/suppliers", SupplierController.GetSupplierByName)
 router.post("/api/suppliers", SupplierController.AddSupplier)
 
-//ANCHOR - INVENTORY ROUTES ===================== >
+//NOTE - INVENTORY ROUTES ===================== >
 router.get("/api/inventory", InventoryController.GetAllInventory)
 
-//ANCHOR - FINANCIAL ELEMENT TYPE ROUTES ===================== >
+//NOTE - FINANCIAL ELEMENT TYPE ROUTES ===================== >
 router.get("/api/elem-types", FinancialElemTypeController.GetAllFinancialElemTypes)
 
-//ANCHOR - TRANSACTION TYPE ROUTES ===================== >
+//NOTE - TRANSACTION TYPE ROUTES ===================== >
 router.get("/api/transaction-types", TransactionTypeController.GetAllTransactionTypes)
 
 module.exports = router;
