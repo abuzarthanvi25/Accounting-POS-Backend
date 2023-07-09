39adac84f15c3d423e0cb065f566fd3cb16bcd34
Abuzar Thanvi <107580489+abuzarthanvi25@users.noreply.github.com>
2023-07-09 20:31:05 +0500

    OPTIMIZED: Create an order function
    - Modified created an order function to be better optimized and give full response as per all changes done
    - Added new variable API response to handle responses
    - Optimized error handling

--- controllers/OrderItemsController.js
+++ controllers/OrderItemsController.js
@@ -75,6 +75,11 @@ const OrderItemsController = {
       }, 0);
 
       let journalPayload = []
+      let apiResponse = {
+        createdOrderItems: [],
+        newSales: [],
+        updatedInventory: []
+      }
 
       journalPayload.push({
         transaction_type_id: await getTransactionTypeModelId(TransactionTypes.Debit),
@@ -132,87 +137,94 @@ const OrderItemsController = {
 
       if(customer_id && sub_total && order_date && order_items && journalResponse){
         //SECTION - Creating order
-        OrderModel.create({
+        const createdOrder = await OrderModel.create({
           customer_id: customer_id,
           order_date: order_date,
           sub_total: sub_total
         }, 
         { transaction }
-        ).then((order) => {
+        )
+
+        if(createdOrder){
           const orderItemsPayload = order_items?.map((val) => ({
-            order_id: order?.id,
+            order_id: createdOrder?.id,
             product_id: val?.product_id,
             quantity: val?.quantity,
             unit_cost: val?.unit_cost,
             unit_price: val?.unit_price,
           }))
-          
-          //SECTION - Creating order items
-          OrderItemsModel.bulkCreate(orderItemsPayload,
-             { transaction }
-             ).then(()=>{
-              orderItemsPayload.map((val)=>{
-                // Handling inventory
-                InventoryModel.decrement(
-                  { quantity_in_stock: val?.quantity},
+
+
+          const createdOrderItems = await OrderItemsModel.bulkCreate(orderItemsPayload,
+            { transaction }
+            )
+
+            apiResponse.createdOrderItems.push(createdOrderItems)
+
+            if(createdOrderItems){
+              for(const orderItem of orderItemsPayload){
+                const updatedInventoryInstance =  await InventoryModel.decrement(
+                  { quantity_in_stock: orderItem?.quantity},
                   {
-                    where: { product_id: val?.product_id },
+                    where: { product_id: orderItem?.product_id },
                     transaction,
                   }
-                ).then(()=>{
-                  // Handling sales
-                  SalesModel.create({
-                    order_id: order?.id,
-                    product_id: val?.product_id,
-                    quantity_sold: val?.quantity,
-                    unit_cost: val?.unit_cost,
-                    unit_price: val?.unit_price,
-                    date: order_date
-                  }, { fields: ['order_id','product_id', 'quantity_sold','unit_cost', 'unit_price', 'date'], transaction }).then((sales)=>{
-                  transaction.commit();
-                  response.json({
-                    message: `Order with order id ${order?.id} created successfully`,
-                    status: true,
-                    order_id: order?.id,
-                    order: orderItemsPayload,
-                    journalEntries: journalResponse
-                  })
-                  }).catch((error)=>{
-                    response.status(400).json({
-                      message: "Error creating order at 106",
-                      status: false,
-                      error: error
-                    });
-                  })
+                )
 
-                  
-                }).catch((error)=>{
+                if(!updatedInventoryInstance){
                   response.status(400).json({
-                    message: "Error creating order at 77",
-                    status: false,
-                    error: error
-                  });
-                })
+                    message: `Error decrementing inventory`,
+                    status: false
+                  })
+                  return
+                }
+
+                apiResponse.updatedInventory.push(updatedInventoryInstance)
+
+                const createdSales = await SalesModel.create({
+                  order_id: createdOrder?.id,
+                  product_id: orderItem?.product_id,
+                  quantity_sold: orderItem?.quantity,
+                  unit_cost: orderItem?.unit_cost,
+                  unit_price: orderItem?.unit_price,
+                  date: order_date
+                }, { fields: ['order_id','product_id', 'quantity_sold','unit_cost', 'unit_price', 'date'], transaction })
+
+                if(!createdSales){
+                  response.status(400).json({
+                    message: `Error creating sales`,
+                    status: false
+                  })
+                  return
+                }
+
+                apiResponse.newSales.push(createdSales)
                 
+              }
 
-                //TODO -  Handling journal entries
+              await transaction.commit();
+              response.json({
+                message: `Order with order id ${createdOrder?.id} created successfully`,
+                status: true,
+                order_id: createdOrder?.id,
+                orderItems: apiResponse.createdOrderItems,
+                sales: apiResponse.newSales,
+                updatedInventory: apiResponse.updatedInventory,
+                journalEntries: journalResponse
               })
-          }).catch((error)=>{
-            response.status(400).json({
-              message: "Error creating order at 93",
-              status: false,
-              error: error
-            });
-          })
-
-
-        }).catch((error) => {
+            }else{
+              response.status(400).json({
+                message: `Error creating order items`,
+                status: false
+              })
+            }
+        }else{
           response.status(400).json({
-            message: "Error creating order at 81",
-            status: false,
-            error: error
-          });
-        })
+            message: `Error creating order`,
+            status: false
+          })
+          return
+        }
           
           
       }
